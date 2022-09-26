f5f649a677a8779dd5d06c72a71320373f129323
Manas Ranjan Murmu <manasranjanmurmu@gmail.com>
2022-09-26 13:55:23 +0530

    Clipboard Assignment Added.

---
+++ Javascript-Project/Clipboard/app.js
@@ -0,0 +1,28 @@
+function clipboard() {
+  let input = document.getElementById("inputvalue").value;
+  console.log(input);
+
+  let ul = document.createElement("ul");
+  ul.setAttribute("class", "unordered");
+
+  let items = document.getElementById("renderedItems");
+  items.setAttribute("style", "display:block");
+  items.appendChild(ul);
+
+  let li = document.createElement("li");
+  li.setAttribute("class", "listitems");
+  li.innerHTML = input;
+  ul.appendChild(li);
+  // let input = document.getElementById("inputvalue").value;
+  // console.log(input);
+  // let items = document.getElementById("renderedItems");
+  // items.setAttribute("style", "display:block");
+  // console.log(items);
+  // let p = document.createElement("p");
+  // p.setAttribute("class", "p");
+  // items.appendChild(p);
+  // p.innerHTML = input;
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", clipboard);
